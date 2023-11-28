d1adff1d54836268b2ce936c336e795c9bc71652
Wajih Nahlé <wajihnahle@Ruzgar.local>
2023-11-27 18:14:09 -0700

    matched simon db file new adds

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('tapbattle');
+const userCollection = db.collection('user');
 const playerCollection = db.collection('players');
 
 (async function testConnection() {
@@ -63,8 +66,33 @@ async function updatePlayer(playerObject) {
       await client.close();
     }
   }
+
+  function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+  
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
   
   module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
     getPlayerInfo,
     addPlayer,
     updatePlayer,
